680a8b476cbf7b884df2b6e19e7996400b367590
Kelvin <kt16@sanger.ac.uk>
2020-05-18 23:50:21 +0100

    trying this

---
+++ dandelion/utilities/_tigger-genotype.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# @Author: Kelvin
+# @Date:   2020-05-18 23:38:05
+# @Last Modified by:   Kelvin
+# @Last Modified time: 2020-05-18 23:47:25
+import dandelion
+import subprocess as sp
+import sys
+
+path = dandelion.__path__[0]
+command = path + "/bin/tigger-genotype.R"
+sp.call([command] + sys.argv)
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/zktuong/dandelion/",
     packages=find_packages(),
     setup_requires=['setuptools_scm'],
-    scripts = ['dandelion/utilities/_pathing.py'],
+    scripts = ['dandelion/utilities/_blastn.py', 'dandelion/utilities/_igblastn.py', 'dandelion/utilities/_makeblastby.py', 'dandelion/utilities/_tigger-genotype.py'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
     classifiers=[
