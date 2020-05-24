aae2a588951cad09280185f1e8feea6e2952ce1b
Kelvin <kt16@sanger.ac.uk>
2020-05-24 10:27:52 +0100

    Update _tools.py
    updated options for transfer_network

--- dandelion/tools/_tools.py
+++ dandelion/tools/_tools.py
@@ -2,7 +2,7 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:22:18
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-05-24 10:20:44
+# @Last Modified time: 2020-05-24 10:26:26
 
 import os
 import scanpy as sc
@@ -850,19 +850,13 @@ def transfer_network(self, network, keep_raw = True, neighbors_key = None):
     if neighbors_key is None:
         neighbors_key = "neighbors"
     if neighbors_key not in self.uns:
-        raise ValueError("`edges=True` requires `pp.neighbors` to be run before.")
-    if neighbors_key in self.raw.uns:
-        if keep_raw:
-            self.raw.uns = copy.deepcopy(self.uns)
-            self.uns[neighbors_key]['connectivities'] = df_connectivities_
-            self.uns[neighbors_key]['distances'] = df_distances_
-            self.uns[neighbors_key]['params'] = {'method':'bcr'}
-        else:
-            self.uns[neighbors_key]['connectivities'] = df_connectivities_
-            self.uns[neighbors_key]['distances'] = df_distances_
-            self.uns[neighbors_key]['params'] = {'method':'bcr'}
-    else:
+        raise ValueError("`edges=True` requires `pp.neighbors` to be run before.")    
+    if keep_raw:
         self.raw.uns = copy.deepcopy(self.uns)
+        self.uns[neighbors_key]['connectivities'] = df_connectivities_
+        self.uns[neighbors_key]['distances'] = df_distances_
+        self.uns[neighbors_key]['params'] = {'method':'bcr'}
+    else:
         self.uns[neighbors_key]['connectivities'] = df_connectivities_
         self.uns[neighbors_key]['distances'] = df_distances_
         self.uns[neighbors_key]['params'] = {'method':'bcr'}
@@ -877,11 +871,17 @@ def transfer_network(self, network, keep_raw = True, neighbors_key = None):
     tmp[[1]] = tmp[[1]]*-1
     X_bcr = np.array(tmp[[0,1]], dtype = np.float32)
     self.obsm['X_bcr'] = X_bcr
-    logg.info(' finished', time=start,
-        deep=('added to `.uns[\'neighbors\']`\n'
-        '   \'distances\', cluster-weighted adjacency matrix\n'
-        '   \'connectivities\', cluster-weighted adjacency matrix\n'
-        'stored original .uns in .raw'))
+    if keep_raw:
+        logg.info(' finished', time=start,
+            deep=('added to `.uns[\''+neighbors_key+'\']`\n'
+            '   \'distances\', cluster-weighted adjacency matrix\n'
+            '   \'connectivities\', cluster-weighted adjacency matrix\n'
+            'stored original .uns in .raw'))
+    else:
+        logg.info(' finished', time=start,
+            deep=('added to `.uns[\''+neighbors_key+'\']`\n'
+            '   \'distances\', cluster-weighted adjacency matrix\n'
+            '   \'connectivities\', cluster-weighted adjacency matrix'))
 
 def create_germlines(self, germline = None, org = 'human', seq_field='sequence_alignment', v_field='v_call', d_field='d_call', j_field='j_call', clone_field='clone_id', germ_types='dmask', fileformat='airr'):
     env = os.environ.copy()
