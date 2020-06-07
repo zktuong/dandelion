66323883f4d61e1d16577b3d8ef113af569eea75
Kelvin <kt16@sanger.ac.uk>
2020-06-08 00:48:04 +0100

    Update __init__.py
    fixed bug

--- dandelion/tools/__init__.py
+++ dandelion/tools/__init__.py
@@ -2,7 +2,6 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:21:45
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-06-07 21:44:11
+# @Last Modified time: 2020-06-08 00:47:55
 
-from . import ext
 from ._tools import filter_bcr, find_clones, generate_network, transfer_network, quantify_mutations, define_clones, calculate_threshold
