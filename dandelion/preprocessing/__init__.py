0d204de9845bfb8b502c359ce08b1c4ec4db99ed
Kelvin <kt16@sanger.ac.uk>
2020-08-07 14:33:26 +0100

    Update __init__.py
    bug fix

--- dandelion/preprocessing/__init__.py
+++ dandelion/preprocessing/__init__.py
@@ -2,7 +2,7 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:42:02
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-07-30 16:42:45
+# @Last Modified time: 2020-08-07 14:33:18
 
 from . import ext
-from ._preprocessing import format_fasta, format_fastas, assign_isotype, reassign_alleles, reannotate_genes, create_germlines, recipe_scanpy_qc, recipe_scanpy_qc_v2, filter_bcr, quantify_mutations, calculate_threshold
+from ._preprocessing import format_fasta, format_fastas, assign_isotype, reassign_alleles, reannotate_genes, create_germlines, recipe_scanpy_qc, filter_bcr, quantify_mutations, calculate_threshold
