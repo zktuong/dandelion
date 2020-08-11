460992ddca150fe1acea819c77fd8e1a3499d9a7
Kelvin <kt16@sanger.ac.uk>
2020-08-11 19:14:13 +0100

    Update __init__.py

--- dandelion/__init__.py
+++ dandelion/__init__.py
@@ -2,10 +2,11 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:11:20
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-06-08 20:52:00
+# @Last Modified time: 2020-08-11 19:10:30
 
 from . import preprocessing as pp
 from . import utilities as utl
 from . import tools as tl
 from . import plotting as pl
+from .utilities import read_pkl
 from .version import __version__
