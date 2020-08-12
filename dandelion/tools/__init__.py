c5087f06e89cc121e1fccdb941558c6533b363a4
Kelvin <kt16@sanger.ac.uk>
2020-08-12 23:24:20 +0100

    Update __init__.py

--- dandelion/tools/__init__.py
+++ dandelion/tools/__init__.py
@@ -2,6 +2,6 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:21:45
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-08-07 23:46:03
+# @Last Modified time: 2020-08-12 18:24:31
 
-from ._tools import find_clones, generate_network, generate_layout, transfer_network, define_clones, quantify_clone_size
+from ._tools import find_clones, generate_network, transfer, define_clones, quantify_clone_size
