5fef7f4bcdc8c96803afe3aa1ee0d6130812ecd3
Kelvin <kt16@sanger.ac.uk>
2020-08-13 00:14:10 +0100

    Update __init__.py
    bug fix

--- dandelion/plotting/__init__.py
+++ dandelion/plotting/__init__.py
@@ -2,6 +2,6 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:21:45
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-07-22 20:26:34
+# @Last Modified time: 2020-08-13 00:14:01
 
-from ._plotting import igraph_network, clone_network, barplot, stackedbarplot, spectratypeplot
+from ._plotting import clone_network, barplot, stackedbarplot, spectratypeplot
