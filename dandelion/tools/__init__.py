f4df994d408cf773cb45e1912424cea0d0ee8a25
Kelvin <kt16@sanger.ac.uk>
2020-08-13 21:39:06 +0100

    Update __init__.py

--- dandelion/tools/__init__.py
+++ dandelion/tools/__init__.py
@@ -2,7 +2,8 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:21:45
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-08-13 15:42:43
+# @Last Modified time: 2020-08-13 21:32:26
 
-from ._tools import find_clones, generate_network, transfer, define_clones, quantify_clone_size
-from ._network import extract_edge_weights
+from ._tools import find_clones, transfer, define_clones, clone_size
+from ._network import extract_edge_weights, clone_degree, generate_network
+from ._diversity import diversity
