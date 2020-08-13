d2b90adb336e8bbc3441ea449a0d8c2875168e2e
Kelvin <kt16@sanger.ac.uk>
2020-08-14 00:56:04 +0100

    Update __init__.py

--- dandelion/tools/__init__.py
+++ dandelion/tools/__init__.py
@@ -2,8 +2,8 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:21:45
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-08-13 21:32:26
+# @Last Modified time: 2020-08-14 00:10:02
 
 from ._tools import find_clones, transfer, define_clones, clone_size
 from ._network import extract_edge_weights, clone_degree, generate_network
-from ._diversity import diversity
+from ._diversity import clone_diversity, rarefaction
