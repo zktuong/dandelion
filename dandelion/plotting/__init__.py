ccec22b6b4e842e1835cbe7338036d60ccd1b061
Kelvin <kt16@sanger.ac.uk>
2020-08-14 00:56:08 +0100

    Update __init__.py

--- dandelion/plotting/__init__.py
+++ dandelion/plotting/__init__.py
@@ -2,6 +2,6 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:21:45
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-08-13 15:13:53
+# @Last Modified time: 2020-08-14 00:14:46
 
-from ._plotting import random_palette, clone_network, barplot, stackedbarplot, spectratypeplot
+from ._plotting import random_palette, clone_network, barplot, stackedbarplot, spectratype, rarefaction
