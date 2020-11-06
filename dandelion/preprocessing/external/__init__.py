bad07d4b21b3b01b48a33fb8bc6241e074c49c1b
Kelvin <kt16@sanger.ac.uk>
2020-11-06 10:49:11 +0000

    Update __init__.py

--- dandelion/preprocessing/external/__init__.py
+++ dandelion/preprocessing/external/__init__.py
@@ -2,6 +2,6 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:42:02
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-10-16 00:35:07
+# @Last Modified time: 2020-11-06 10:46:41
 
-from ._preprocessing import assigngenes_igblast, makedb_igblast, tigger_genotype, insertGaps, recipe_scanpy_qc
+from ._preprocessing import assigngenes_igblast, makedb_igblast, tigger_genotype, recipe_scanpy_qc
