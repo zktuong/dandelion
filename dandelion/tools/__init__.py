6c04590e07b74fd8ed8310ff785d712651a13bca
Kelvin <kt16@sanger.ac.uk>
2020-12-16 15:50:32 +0000

    trying a new parameter

--- dandelion/tools/__init__.py
+++ dandelion/tools/__init__.py
@@ -2,8 +2,8 @@
 # @Author: Kelvin
 # @Date:   2020-05-13 23:21:45
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2020-11-18 19:54:50
+# @Last Modified time: 2020-12-16 15:48:54
 
 from ._tools import find_clones, transfer, define_clones, clone_size, clone_overlap
-from ._network import extract_edge_weights, clone_degree, clone_centrality, generate_network
+from ._network import extract_edge_weights, clone_degree, clone_centrality, generate_network, clone_voterank
 from ._diversity import clone_diversity, clone_rarefaction
