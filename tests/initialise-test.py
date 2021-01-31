a3a4436e20ffd9c172d4de46c87c8bfe57bad0e5
Kelvin <kt16@sanger.ac.uk>
2021-01-31 23:13:31 +0000

    Create initialise-test.py

---
+++ tests/initialise-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# basic requirements for test data
+import sys
+import dandelion as ddl
+import pandas as pd
+import requests
+from io import StringIO
+
+def test():
+	file = 'https://cf.10xgenomics.com/samples/cell-vdj/5.0.0/sc5p_v2_hs_B_1k_multi_5gex_b/sc5p_v2_hs_B_1k_multi_5gex_b_vdj_b_airr_rearrangement.tsv'
+	r = requests.get(file)
+	test_data = pd.read_csv(StringIO(r.text), sep = '\t')
+	test_data['locus'] = ['IGH' if 'IGH' in i else 'IGK' if 'IGK' in i else 'IGL' if 'IGL' in i else None for i in test_data.v_call]
+	test_data['sample_id'] = '10X'
+	test_ddl = ddl.Dandelion(test_data)
+	test_ddl.write_h5('test.h5')
+
+if __name__ == '__main__':
+	test()
