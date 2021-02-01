311364a83f4ea77c891b5495a1eff7e7789f8787
Kelvin <kt16@sanger.ac.uk>
2021-02-01 01:36:37 +0000

    Update test_IO.py

--- tests/test_IO.py
+++ tests/test_IO.py
@@ -6,19 +6,19 @@ import pandas as pd
 import requests
 from io import StringIO
 
-def init_test():
+def test_init():
 	file = 'https://cf.10xgenomics.com/samples/cell-vdj/5.0.0/sc5p_v2_hs_B_1k_multi_5gex_b/sc5p_v2_hs_B_1k_multi_5gex_b_vdj_b_airr_rearrangement.tsv'
 	r = requests.get(file)
 	test_data = pd.read_csv(StringIO(r.text), sep = '\t')
 	test_data['locus'] = ['IGH' if 'IGH' in i else 'IGK' if 'IGK' in i else 'IGL' if 'IGL' in i else None for i in test_data.v_call]
 	test_data['sample_id'] = '10X'
-	test_ddl = ddl.Dandelion(test_data)
-	test_ddl.write_h5('test.h5')
-
-def read_test():
-	test_ddl_2 = ddl.read_h5('test.h5')
-	print(test_ddl_2)
+	test_ddl = ddl.Dandelion(test_data)	
+	test_ddl.write_h5('test.h5', compression = 'bzip2')
+	test_ddl.write_pkl('test.pkl.pbz2')
+	test2 = ddl.read_h5('test.h5')
+	test3 = ddl.read_h5('test.pkl.pbz2')
+	print(test2)
+	print(test3)
 
 if __name__ == '__main__':
-	init_test()
-	read_test()
+	test_init()
