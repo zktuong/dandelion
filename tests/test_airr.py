09eac5b49808e04f16759d659b1f8684d87373a9
Kelvin <kt16@sanger.ac.uk>
2021-02-01 09:02:26 +0000

    fix name mistake

--- tests/test_airr.py
+++ tests/test_airr.py
@@ -8,12 +8,12 @@ import pandas as pd
 import requests
 from io import StringIO
 
-def test_IO():
+def test_init():
 	file = 'https://cf.10xgenomics.com/samples/cell-vdj/5.0.0/sc5p_v2_hs_B_1k_multi_5gex_b/sc5p_v2_hs_B_1k_multi_5gex_b_vdj_b_airr_rearrangement.tsv'
 	r = requests.get(file)
 	test_data = pd.read_csv(StringIO(r.text), sep = '\t')
 	test_data['locus'] = ['IGH' if 'IGH' in i else 'IGK' if 'IGK' in i else 'IGL' if 'IGL' in i else None for i in test_data.v_call]
-	test_data['umi_count'] = test_data['duiplicate_count']
+	test_data['umi_count'] = test_data['duplicate_count']
 	test_data['sample_id'] = '10X'
 	test_ddl = ddl.Dandelion(test_data)
 	test_ddl.write_h5('test.h5', compression = 'bzip2')
@@ -56,7 +56,7 @@ def test_downsampling():
 	print(test_downsample)
 
 if __name__ == '__main__':
-	test_IO()
+	test_init()
 	test_scanpy()
 	test_filter()
 	test_update_metadata()
