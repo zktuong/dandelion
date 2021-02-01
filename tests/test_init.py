c69b783f20db956d42ceb88db2267b73d46f1986
Kelvin <kt16@sanger.ac.uk>
2021-02-01 02:57:25 +0000

    toggle tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # basic requirements for test data
 import sys
+import os
 import dandelion as ddl
+import scanpy as sc
 import pandas as pd
 import requests
 from io import StringIO
@@ -19,6 +21,21 @@ def test_IO():
 	_ = ddl.read_pkl('test.pkl.pbz2')
 	print(test)
 
+def test_scanpy():
+	scfile = 'https://cf.10xgenomics.com/samples/cell-vdj/5.0.0/sc5p_v2_hs_B_1k_multi_5gex_b/sc5p_v2_hs_B_1k_multi_5gex_b_count_filtered_feature_bc_matrix.h5' 
+	r = requests.get(scfile)
+	open('sctest.h5', 'wb').write(r.content)
+	adata = sc.read_10x_h5('sctest.h5')
+	print(adata)
+
+def test_filter():
+	adata = sc.read_10x_h5('sctest.h5')
+	test = ddl.read_h5('test.h5')
+	adata.obs['filter_rna'] = False
+	test, adata = ddl.pp.filter_bcr(test, adata)
+	adata.write('sctest.h5ad', compression = 'gzip')
+	test.write_h5('test.h5', compression = 'bzip2')
+
 def test_update_metadata():
 	test = ddl.read_h5('test.h5')
 	ddl.update_metadata(test, 'sequence_id')
@@ -27,31 +44,22 @@ def test_find_clones():
 	test = ddl.read_h5('test.h5')
 	ddl.tl.find_clones(test)
 
-def test_define_clones():
-	test = ddl.read_h5('test.h5')
-	ddl.pp.calculate_threshold(test)
-	ddl.tl.define_clones(test, key_added = 'changeo_clone_id')
-
 def test_generate_network():
 	test = ddl.read_h5('test.h5')
-	ddl.tl.generate_network(test)
+	ddl.tl.generate_network(test, key = 'sequence_alignment')
 
 def test_downsampling():
 	test = ddl.read_h5('test.h5')
-	ddl.tl.generate_network(test)
-	test_downsample = ddl.tl.generate_network(test, downsample = 500)
+	ddl.tl.generate_network(test, key = 'sequence_alignment')
+	test_downsample = ddl.tl.generate_network(test, key = 'sequence_alignment', downsample = 500)
 	print(test_downsample)
-
-def test_quantify_mutation():
-	test = ddl.read_h5('test.h5')
-	ddl.pp.quantify_mutations(test)
 
 if __name__ == '__main__':
 	test_IO()
+	test_scanpy()
+	test_filter()
 	test_update_metadata()
 	test_find_clones()
-	test_define_clones()
 	test_generate_network()
 	test_downsampling()
-	test_quantify_mutation()
 
