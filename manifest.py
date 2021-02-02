f510c799b14f176bcce07cbfad1e66951cea20e4
Kelvin <kt16@sanger.ac.uk>
2021-02-02 11:10:38 +0000

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -4,16 +4,15 @@ Generates the version number file as manifest
 '''
 
 import json
+exec(open('dandelion/version.py').read())
 
 def main():
-	exec(open('dandelion/version.py').read())
-	data = {}
-	data['version'] = []
-	data['version'].append({
-    	'version': __version__
-	})
-	with open('manifest.json', 'w') as outfile:
-    	json.dump(data, outfile)
+    data = {}
+    data['version'] = []
+    data['version'].append({'version': __version__})
+    with open('manifest.json', 'w') as outfile:
+        json.dump(data, outfile)
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":    
     main()
