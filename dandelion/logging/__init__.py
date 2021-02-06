a0876cd51d0f080d5129c8da432b06ce4c0af802
Kelvin <kt16@sanger.ac.uk>
2021-02-06 13:26:28 +0000

    Create __init__.py

---
+++ dandelion/logging/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# @Author: kt16
+# @Date:   2020-05-12 18:11:20
+# @Last Modified by:   Kelvin
+# @Last Modified time: 2020-11-23 15:43:34
+
+from ._logging import print_header
