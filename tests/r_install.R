289fba0d95e676d8ec3a7ad63441d64314239ecb
Kelvin <kt16@sanger.ac.uk>
2021-02-07 22:52:00 +0000

    change the order of installation

--- tests/r_install.R
+++ tests/r_install.R
@@ -1,4 +1,1 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install(c("Biostrings","GenomicAlignments", "IRanges"))
 install.packages(c('shazam', 'alakazam', 'tigger', 'airr', 'optparse'))
