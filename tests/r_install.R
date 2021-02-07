5df3d19d5bd54439ac1434e050a837ae1c9d9f87
Kelvin <kt16@sanger.ac.uk>
2021-02-07 19:52:03 +0000

    see if this is the issue failing the tests

--- tests/r_install.R
+++ tests/r_install.R
@@ -1,1 +1,4 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(c("Biostrings","GenomicAlignments", "IRanges"))
 install.packages(c('shazam', 'alakazam', 'tigger', 'airr', 'optparse'))
