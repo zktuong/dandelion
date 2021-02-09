342e191e5f99ec7fddf6ca1f5c999bf320d422d0
Kelvin <kt16@sanger.ac.uk>
2021-02-09 13:20:03 +0000

    Update __init__.py

--- dandelion/__init__.py
+++ dandelion/__init__.py
@@ -2,12 +2,12 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:11:20
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-02-06 21:55:16
+# @Last Modified time: 2021-02-09 13:19:29
 
 from . import preprocessing as pp
 from . import utilities as utl
 from . import tools as tl
 from . import plotting as pl
-from .utilities import read_pkl, read_h5, read_10x_airr, Dandelion, update_metadata, concat, load_data
+from .utilities import read_pkl, read_h5, read_10x_airr, read_scirpy, to_scirpy, Dandelion, update_metadata, concat, load_data
 from .version import __version__
 from . import logging
