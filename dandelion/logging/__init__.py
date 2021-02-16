b120134c02a352fe71b21e039d1f42a58255c77f
Kelvin <kt16@sanger.ac.uk>
2021-02-16 16:00:39 +0000

    Update __init__.py

--- dandelion/logging/__init__.py
+++ dandelion/logging/__init__.py
@@ -2,6 +2,6 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:11:20
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-02-16 15:57:45
+# @Last Modified time: 2021-02-16 15:59:05
 
-from ._logging import print_versions
+from ._logging import print_versions, print_header
