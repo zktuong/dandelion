6576915cbbd3c335d086f3f76c18e83af3fbb71c
Kelvin <kt16@sanger.ac.uk>
2021-03-31 16:56:30 +0100

    Update version.py

--- dandelion/version.py
+++ dandelion/version.py
@@ -1,4 +1,7 @@
-exec(open('dandelion/logging/_scmtag.py').read())
+try:
+	exec(open('dandelion/logging/_scmtag.py').read())
+except:
+	exec(open('../dandelion/logging/_scmtag.py').read())
 
 if '+' in version:
 	__version__ = version.split('+')[0]
