791efad09f66b24120b7681d64e4ab87e337d745
Kelvin <kt16@sanger.ac.uk>
2021-04-03 17:43:33 +0100

    test

--- dandelion/logging/_badge.py
+++ dandelion/logging/_badge.py
@@ -1,2 +1,4 @@
-from .version import __version__
-print(__version__)
+from version import __version__
+# badge = __version__.split('dev')[0]+'dev'+str(int(__version__.split('+')[0].split('dev')[1])+1)
+badge = __version__
+print(badge)
