f211cf6d24f3d96be5d74d4f7933c4e990ed7348
Kelvin <kt16@sanger.ac.uk>
2021-04-03 18:13:44 +0100

    hmm try this?

--- dandelion/logging/_badge.py
+++ dandelion/logging/_badge.py
@@ -1,4 +1,2 @@
-from version import __version__
-# badge = __version__.split('dev')[0]+'dev'+str(int(__version__.split('+')[0].split('dev')[1])+1)
-badge = __version__
-print(badge)
+from dandelion import __version__
+print(__version__)
