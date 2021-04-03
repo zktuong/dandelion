cc70996352ec21f6efad5e299f77a259c766aeaa
Kelvin <kt16@sanger.ac.uk>
2021-04-03 15:15:07 +0100

    Fixed docs and badge

--- dandelion/version.py
+++ dandelion/version.py
@@ -1,5 +1,10 @@
 from setuptools_scm import get_version
-version = get_version()
+try:
+	version = get_version()
+except:
+	import os
+	os.chdir('..')
+	version = get_version()
 
 if '+' in version:
 	__version__ = version.split('+')[0]
