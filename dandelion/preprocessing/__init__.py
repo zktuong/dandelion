7216995e38b646de0d40fbd3129ec8df63f31227
Kelvin <kt16@sanger.ac.uk>
2021-05-14 13:19:26 +0100

    Update __init__.py

--- dandelion/preprocessing/__init__.py
+++ dandelion/preprocessing/__init__.py
@@ -2,8 +2,8 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:42:02
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-02-10 13:57:40
+# @Last Modified time: 2021-05-14 13:14:56
 
 from . import external
-from ._preprocessing import format_fasta, format_fastas, assign_isotype, assign_isotypes, reassign_alleles, reassign_alleles_, reannotate_genes, create_germlines, filter_bcr, quantify_mutations, calculate_threshold
+from ._preprocessing import format_fasta, format_fastas, assign_isotype, assign_isotypes, reassign_alleles, reannotate_genes, create_germlines, filter_bcr, quantify_mutations, calculate_threshold
 from .external._preprocessing import recipe_scanpy_qc
