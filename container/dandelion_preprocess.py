570d3d42728a7d033610cc26bb639c6e86c50609
ktpolanski <krzysztof_polanski@o2.pl>
2021-05-17 13:40:32 +0100

    only prepend sample ID if more than one sample

--- container/dandelion_preprocess.py
+++ container/dandelion_preprocess.py
@@ -12,7 +12,7 @@ def parse_args():
 	'''
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--meta', help='Optional metadata CSV file, header required, first column for sample ID matching folder names in the directory this is being ran in. Can have a "prefix"/"suffix" column for barcode alteration, and "individual" to provide tigger groupings that isn\'t analysing all of the samples jointly.')
-	parser.add_argument('--sep', type=str, default="_", help='The separator to place between the barcode and prefix/suffix. Defaults to "_". Uses sample names as a prefix if metadata CSV file absent.')
+	parser.add_argument('--sep', type=str, default="_", help='The separator to place between the barcode and prefix/suffix. Defaults to "_". Uses sample names as a prefix if metadata CSV file absent and more than one sample to process.')
 	parser.add_argument('--keep_trailing_hyphen_number', action='store_false', help='If passed, do not strip out the trailing hyphen number, e.g. "-1", from the end of barcodes.')
 	parser.add_argument('--clean_output', action='store_true', help='If passed, remove intermediate files that aren\'t the primary output from the run reults. The intermediate files may be occasionally useful for inspection.')
 	args = parser.parse_args()
@@ -48,8 +48,12 @@ def main():
 		vals = list(meta['suffix'].values)
 		ddl.pp.format_fastas(samples, suffix=vals, sep=args.sep, remove_trailing_hyphen_number=args.keep_trailing_hyphen_number)
 	else:
-		#neither. tag with the sample names as default
-		ddl.pp.format_fastas(samples, prefix=samples, sep=args.sep, remove_trailing_hyphen_number=args.keep_trailing_hyphen_number)
+		#neither. tag with the sample names as default, if more than one sample
+		if len(samples) > 1:
+			ddl.pp.format_fastas(samples, prefix=samples, sep=args.sep, remove_trailing_hyphen_number=args.keep_trailing_hyphen_number)
+		else:
+			#no need to tag as it's a single sample.
+			ddl.pp.format_fastas(samples, remove_trailing_hyphen_number=args.keep_trailing_hyphen_number)
 	
 	#STEP TWO - ddl.pp.reannotate_genes()
 	#no tricks here
