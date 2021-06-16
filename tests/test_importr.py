c191a461c6ac4e57f3ec24df03d01709b180b3f8
Kelvin <kt16@sanger.ac.uk>
2021-06-16 14:17:03 +0100

    Update test_importr.py

--- tests/test_importr.py
+++ tests/test_importr.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
-import rpy2
-from rpy2.robjects.packages import importr
 
+def test_importrpy2():
+    try:
+        from rpy2.robjects.packages import importr
+        from rpy2.rinterface import NULL
+        from rpy2.robjects import pandas2ri
+    except:
+        raise (ImportError(
+            "Unable to initialise R instance. Please run this separately through R with Shazam's tutorial."
+        ))
 
-def test_importrpy2():    
     sh = importr('shazam')
     assert sh.__module__ == 'rpy2.robjects.packages'
