aed8ef30bd679d72c0484c7d6888557ecafaeb3d
Kelvin <kt16@sanger.ac.uk>
2021-06-16 10:01:20 +0100

    Update test_importr.py
    definitely something wrong here

--- tests/test_importr.py
+++ tests/test_importr.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+import rpy2
+from rpy2.robjects.packages import importr
 
-def test_importrpy2():
-    import rpy2
-    from rpy2.robjects.packages import importr
+
+def test_importrpy2():    
     sh = importr('shazam')
     assert sh.__module__ == 'rpy2.robjects.packages'
