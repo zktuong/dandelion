d9624c291ba1a36da4c18b3d4597fd7b68e911c1
Kelvin <kt16@sanger.ac.uk>
2021-07-18 17:37:09 +0100

    Update _metadata.py

--- dandelion/_metadata.py
+++ dandelion/_metadata.py
@@ -2,37 +2,22 @@
 # @Author: Kelvin
 # @Date:   2021-04-03 16:46:13
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-07-18 17:29:33
+# @Last Modified time: 2021-07-18 17:36:46
 
 # try:
 #   from .version import __version__
 # except ImportError:
 #   __version__ = ''
-from pathlib import Path
 
-here = Path(__file__).parent
-
-try:
-    from setuptools_scm import get_version
-    import pytoml
-    proj = pytoml.loads((here.parent / "pyproject.toml").read_text())
-
-    __version__ = get_version(root="..", relative_to=__file__, **proj["tool"]["setuptools_scm"])
-    __author__ = "Zewen Kelvin Tuong"
-    __email__ = "kt16@sanger.ac.uk"
-    __classifiers__ = [
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: R",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-    ]
-except:
-    try:
-        from importlib.metadata import metadata as m
-    except ImportError:
-        from importlib_metadata import metadata as m
-
-    metadata = m(here.name)
-    __version__ = metadata["Version"]
-    __author__ = metadata["Author"]
-    __email__ = metadata["Author-email"]
+from setuptools_scm import get_version
+__version__ = get_version(root="..",
+                          relative_to=__file__,
+                          git_describe_command = "git describe --dirty --tags --long --match v*.*.*").split('+')[0]
+__author__ = "Zewen Kelvin Tuong"
+__email__ = "kt16@sanger.ac.uk"
+__classifiers__ = [
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: R",
+    "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+]
