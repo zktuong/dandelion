c122e907bac19404ceda6cc94097ca8226534962
Kelvin <kt16@sanger.ac.uk>
2021-07-18 17:54:05 +0100

    revert to original method

--- dandelion/logging/__init__.py
+++ dandelion/logging/__init__.py
@@ -2,6 +2,7 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:11:20
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-07-18 17:14:49
+# @Last Modified time: 2021-07-18 17:52:06
 
 from ._logging import print_versions, print_header
+from ._metadata import __version__, __author__, __email__, __classifiers__
