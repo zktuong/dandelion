4843ca31e22fbb636263d5315926d13a02fc8642
Kelvin <kt16@sanger.ac.uk>
2021-07-18 17:16:30 +0100

    try and fix badge

--- dandelion/_metadata.py
+++ dandelion/_metadata.py
@@ -2,7 +2,7 @@
 # @Author: Kelvin
 # @Date:   2021-04-03 16:46:13
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-07-18 17:10:57
+# @Last Modified time: 2021-07-18 17:14:24
 
 # try:
 # 	from .version import __version__
@@ -10,10 +10,8 @@
 # 	__version__ = ''
 
 from setuptools_scm import get_version
-try:
-    __version__ = get_version(root="../..", relative_to=__file__).split('+')[0]
-except:
-    __version__ = get_version(root="..", relative_to=__file__).split('+')[0]
+
+__version__ = get_version(root="..", relative_to=__file__).split('+')[0]
 __author__ = "Zewen Kelvin Tuong"
 __email__ = "kt16@sanger.ac.uk"
 __url__ = "https://www.github.com/zktuong/dandelion/"
