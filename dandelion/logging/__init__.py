360a550da85e8d15004cb61af65dc717ba1a29a6
Kelvin <kt16@sanger.ac.uk>
2021-07-18 17:16:30 +0100

    try and fix badge

--- dandelion/logging/__init__.py
+++ dandelion/logging/__init__.py
@@ -2,7 +2,6 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:11:20
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-04-03 16:53:10
+# @Last Modified time: 2021-07-18 17:14:49
 
 from ._logging import print_versions, print_header
-from ._metadata import __version__, __author__, __email__, __url__, __docs__, __classifiers__
