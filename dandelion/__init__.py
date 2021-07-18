c5aa574da85d40dcf5f5598ae03896716e0f4fa3
Kelvin <kt16@sanger.ac.uk>
2021-07-18 17:22:01 +0100

    try again

--- dandelion/__init__.py
+++ dandelion/__init__.py
@@ -2,12 +2,12 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:11:20
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-07-18 17:15:07
+# @Last Modified time: 2021-07-18 17:21:33
 
 from . import preprocessing as pp
 from . import utilities as utl
 from . import tools as tl
 from . import plotting as pl
 from .utilities import read_pkl, read_h5, read_10x_airr, read_10x_vdj, from_scirpy, to_scirpy, Dandelion, update_metadata, concat, load_data
-from ._metadata import __version__, __author__, __email__, __url__, __docs__, __classifiers__
+from ._metadata import __version__, __author__, __email__, __classifiers__
 from . import logging
