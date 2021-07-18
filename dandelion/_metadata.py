6295a6cbed5b0fa8e2f8dfc45c77ac6fb9daed68
Kelvin <kt16@sanger.ac.uk>
2021-07-18 17:22:01 +0100

    try again

--- dandelion/_metadata.py
+++ dandelion/_metadata.py
@@ -2,23 +2,37 @@
 # @Author: Kelvin
 # @Date:   2021-04-03 16:46:13
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-07-18 17:14:24
+# @Last Modified time: 2021-07-18 17:21:08
 
 # try:
-# 	from .version import __version__
+#   from .version import __version__
 # except ImportError:
-# 	__version__ = ''
+#   __version__ = ''
+from pathlib import Path
 
-from setuptools_scm import get_version
+here = Path(__file__).parent
 
-__version__ = get_version(root="..", relative_to=__file__).split('+')[0]
-__author__ = "Zewen Kelvin Tuong"
-__email__ = "kt16@sanger.ac.uk"
-__url__ = "https://www.github.com/zktuong/dandelion/"
-__docs__ = "https://sc-dandelion.readthedocs.io/"
-__classifiers__ = [
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: R",
-    "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-]
+try:
+    from setuptools_scm import get_version
+    import pytoml
+    proj = pytoml.loads((here.parent / "pyproject.toml").read_text())
+
+    __version__ = get_version(root="..", relative_to=__file__, **proj["tool"]["setuptools_scm"])
+    __author__ = "Zewen Kelvin Tuong"
+    __email__ = "kt16@sanger.ac.uk"
+    __classifiers__ = [
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: R",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ]
+except:
+    try:
+        from importlib.metadata import metadata as m
+    except ImportError:
+        from importlib_metadata import metadata as m
+
+    metadata = m(here.name)
+    __version__ = metadata["Version"]
+    __author__ = metadata["Author"]
+    __email__ = metadata["Author-email"]
