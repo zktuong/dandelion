9e5261c02f2fed634cf835b31183f0e11868833a
Kelvin <kt16@sanger.ac.uk>
2021-07-18 17:54:05 +0100

    revert to original method

--- dandelion/__init__.py
+++ dandelion/__init__.py
@@ -2,12 +2,12 @@
 # @Author: kt16
 # @Date:   2020-05-12 18:11:20
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-07-18 17:21:33
+# @Last Modified time: 2021-07-18 17:51:51
 
 from . import preprocessing as pp
 from . import utilities as utl
 from . import tools as tl
 from . import plotting as pl
 from .utilities import read_pkl, read_h5, read_10x_airr, read_10x_vdj, from_scirpy, to_scirpy, Dandelion, update_metadata, concat, load_data
-from ._metadata import __version__, __author__, __email__, __classifiers__
+from .logging import __version__, __author__, __email__, __classifiers__
 from . import logging
--- dandelion/logging/_metadata.py
+++ dandelion/logging/_metadata.py
@@ -2,17 +2,15 @@
 # @Author: Kelvin
 # @Date:   2021-04-03 16:46:13
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-07-18 17:36:46
+# @Last Modified time: 2021-07-18 17:51:13
 
-# try:
-#   from .version import __version__
-# except ImportError:
-#   __version__ = ''
-
-from setuptools_scm import get_version
-__version__ = get_version(root="..",
-                          relative_to=__file__,
-                          git_describe_command = "git describe --dirty --tags --long --match v*.*.*").split('+')[0]
+try:
+    from setuptools_scm import get_version
+    __version__ = get_version(root="..",
+                              relative_to=__file__,
+                              git_describe_command = "git describe --dirty --tags --long --match v*.*.*").split('+')[0]
+except:
+    from .version import __version__
 __author__ = "Zewen Kelvin Tuong"
 __email__ = "kt16@sanger.ac.uk"
 __classifiers__ = [
