f851323a61246193e93cadcc1d4ba68268d057d7
Kelvin <kt16@sanger.ac.uk>
2021-08-02 09:27:16 +0100

    Revert "wanted a more complicated test but can't because it's not writable."
    This reverts commit 00542519bc2ab69cea7994a35718a53fbd4ab8eb.

--- container/tests/test_container.py
+++ container/tests/test_container.py
@@ -1,4 +1,22 @@
 #!/opt/conda/envs/dandelion/bin/python
-def test_import():
-    import dandelion as ddl
-    ddl.logging.print_header()
+import os
+import pandas as pd
+import dandelion as ddl
+
+
+def test_container():
+    os.system(
+        "cd /share/tests/; python /share/dandelion_preprocess.py --meta test.csv;")
+    dat = pd.read_csv(
+        '/share/tests/sample_test_10x/dandelion/filtered_contig_igblast_db-pass_genotyped.tsv',
+        sep='\t')
+    assert not dat['c_call'].empty
+    assert not dat['v_call_genotyped'].empty
+    assert not dat['mu_count'].empty
+    assert not dat['mu_freq'].empty
+    vdj = None
+    try:
+        vdj = ddl.Dandelion(dat)
+    except:
+        pass
+    assert vdj is not None
