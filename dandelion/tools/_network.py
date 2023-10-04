ddcb87f2eebc4022588d78f1cc941f8566440e9e
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2023-10-04 16:49:17 +1000

    shh

--- dandelion/tools/_network.py
+++ dandelion/tools/_network.py
@@ -222,7 +222,7 @@ def generate_network(
                     if c != "None":
                         tmp_clusterdist[c][i].value = 1
             else:
-                cx = out.metadata.loc[i, str(clonekey)]
+                cx = out.metadata.loc[i, str(clonekey)].copy()
                 if cx != "None":
                     tmp_clusterdist[cx][i].value = 1
         tmp_clusterdist2 = {}
