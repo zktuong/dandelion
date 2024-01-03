c41ec3e1fa159b68318af5137a77f6589828dffd
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2024-01-03 21:01:05 +1000

    oops

--- container/setup_mamba.sh
+++ container/setup_mamba.sh
@@ -5,7 +5,12 @@ bash Miniforge3-$(uname)-$(uname -m).sh -b -p "/opt/conda"
 rm Miniforge3-$(uname)-$(uname -m).sh
 . /opt/conda/etc/profile.d/conda.sh
 . /opt/conda/etc/profile.d/mamba.sh
-mamba env update --name sc-dandelion-container -f environment.yml
+# fix pathing
 echo ". /opt/conda/etc/profile.d/conda.sh" | tee -a $SINGULARITY_ENVIRONMENT
 echo ". /opt/conda/etc/profile.d/mamba.sh" | tee -a $SINGULARITY_ENVIRONMENT
 echo "conda activate sc-dandelion-container" | tee -a $SINGULARITY_ENVIRONMENT
+echo "export GERMLINE=/share/database/germlines/" | tee -a $SINGULARITY_ENVIRONMENT
+echo "export IGDATA=/share/database/igblast/" | tee -a $SINGULARITY_ENVIRONMENT
+echo "export BLASTDB=/share/database/blast/" | tee -a $SINGULARITY_ENVIRONMENT
+chmod +x /share/dandelion_preprocess.py
+chmod +x /share/changeo_clonotypes.py
