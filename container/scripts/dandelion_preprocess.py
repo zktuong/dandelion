22d3e2683adfa00e67cd8b7f00438ee6f55da735
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2024-01-10 22:44:57 +1000

    Update dandelion_preprocess.py

--- container/scripts/dandelion_preprocess.py
+++ container/scripts/dandelion_preprocess.py
@@ -353,7 +353,7 @@ def main():
             )
             if args.skip_tigger:
                 ddl.pp.create_germlines(
-                    airr_file=samp_path,
+                    vdj_data=samp_path,
                     org=args.org,
                     db=args.db,
                     strain=args.strain,
